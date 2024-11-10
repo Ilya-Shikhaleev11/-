6236e41d08db29635b05c88a07ef5c6ab5a60fea
ilya shikhaleev <89536869140i@gmail.com>
2024-11-10 21:41:43 +0300

    Первая домашняя работа через githab

---
+++ main.js
@@ -0,0 +1,41 @@
+// var name = prompt("Как ваше имя? ");
+// alert("Привет, " + name +"!");
+
+// var year_of_birth = Number(prompt("Ваша год рождения? "));
+// const this_year = 2024
+// alert("В этом году вам  " + (this_year - year_of_birth) + " лет ");
+
+// var side = Number(prompt("Введите длину стороны квадрата"));
+// alert("Периметр:"+(side*4) + " ");
+
+// var r = Number(prompt("Введите радиус окружности "));
+// alert("Радиус окружности:"+(3.14*r*r) + " ");
+
+// let distance = prompt("введите расстояние в км")
+// let time = prompt("введите время")
+// alert(distance/time + "км/ч")
+
+// const euro = 0.93
+// var dollar = Number(prompt("Введите количество долларов для обмена: "));
+// alert("При обмене вы получите " + dollar * euro + " евро"); 
+
+// var flash  = prompt("Укажыте объем флешки в Гб: ");
+// alert( "Файлов размером в 820 Мб помещается на флешку" + (flash*1024)/820 + "файла");
+
+// var wallet = prompt("введите сумму денег в кошельке")
+// var chocolate = prompt("введите сумму денег одной шоколадки")
+// var rez = wallet/chocolate
+// var change =wallet-(rez*chocolate) 
+// alert("пользователь может купить " + rez + "шоколадок");
+// alert("сдачи у него останется" + change + "рублей");
+
+// var nmb = prompt("Введите трёхзначное число: ");
+// var a = Number(nmb % 10);
+// var b = Number((nmb - a) / 10 % 10);
+// var c = Number((nmb - a - b * 10) / 100);
+// alert("Перевёртыш: " + (a * 100 + b * 10 + c));
+
+// var a = prompt("Введите число для проверки на чётность: ")
+// a = Number(a)
+// alert((parseInt(a) % 2 == 0) ? "ЧЁТНОЕ" : "НЕЧЁТНОЕ")
+
